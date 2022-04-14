778e2d7841e9948a4d03ce3676378032af54ada5
Vadim Raskhozhev <iamdexpl@gmail.com>
2022-04-14 13:38:45 +0300

    Cosmetic changes

--- winrm_setup.bat
+++ winrm_setup.bat
@@ -2,9 +2,9 @@
 
 powershell -NoLogo -Command "& {Set-NetConnectionProfile -NetworkCategory Private}"
 call winrm quickconfig -q -force
-# https://github.com/ansible/ansible/issues/42978
+:: https://github.com/ansible/ansible/issues/42978
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /f /v LocalAccountTokenFilterPolicy /d 1 /t REG_DWORD
+:: Don't go any further on AD machines
 wmic computersystem where name="%computername%" get partofdomain /format:value | findstr -c:"^partofdomain=true$" -ir && exit /b
 call winrm set winrm/config/service @{AllowUnencrypted="true"}
-:: Allow basic auth only on non-AD machines
 call winrm set winrm/config/service/auth @{Basic="true"}
